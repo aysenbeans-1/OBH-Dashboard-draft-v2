4b650eebfe06cb4a2cfa5ec422db827a04917186
aysenbeans-1 <evdabordo@gmail.com>
2026-08-03 09:12:39 +0800

    feat(build): configure application for WAR deployment
    Updated the project to support deployment as a WAR file by adding the tomcat dependency, extending SpringBootServletInitializer, and configuring the build packaging. Added JWT_SECRET to environment configuration for upcoming authentication implementation.

--- core-java/src/main/java/com/obhdashboard/middleware/MiddlewareApplication.java
+++ core-java/src/main/java/com/obhdashboard/middleware/MiddlewareApplication.java
@@ -2,9 +2,17 @@ package com.obhdashboard.middleware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MiddlewareApplication {
+public class MiddlewareApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MiddlewareApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MiddlewareApplication.class, args);
     }
